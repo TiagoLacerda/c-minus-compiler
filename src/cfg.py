e941d4ad2850c33666453c2a7a79d70cbcf8cf1f
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-11-16 16:04:34 -0300

    removal of the left-most recursion

--- src/cfg.py
+++ src/cfg.py
@@ -1,10 +1,13 @@
 from typing import *
 import json
 from collections import deque
+from copy import deepcopy
 
 state_set  = Set[ str ]
 state_seq  = List[ str ]
 transition = Dict[ str , List[ state_seq ] ]
+
+EPSILON = '\u03B5'
 
 class CFG:
 
@@ -63,6 +66,17 @@ class CFG:
         self.terminals = terminals
         self.derivations = derivations
         self.name = name
+
+        #---------------------------------------------------
+        # Checking for left_recursion. ie if there any derivation
+        # like A :: A B | A a, where a is terminal and A and B are
+        # states
+        self.left_recursion = False
+        for state in states:
+            for rule in derivations[ state ]:
+                if rule[ 0 ] == state:
+                    self.left_recursion = True
+                    break
 
     ######## DATA REPRESENTATION #########
     def to_dict( self ) -> Dict[ str , Any ]:
@@ -134,7 +148,7 @@ class CFG:
     @staticmethod
     def from_file( filename ):
         with open( filename , "r" ) as f:
-            return CFG.from_json( f.readline() )
+            return CFG.from_json( f.read() )
     
     def __str__( self ) -> str:
         
@@ -145,7 +159,7 @@ class CFG:
 
         transition_str : str = ""
         states : Deque[ str ] = deque( [ self.root ] )
-        visited : state_set = set() 
+        visited : state_set = { self.root } 
 
         while states:  # while there are elements in the queue
 
@@ -153,7 +167,6 @@ class CFG:
             # fetching next state and transitions
             state = states.popleft()
             transitions = self.derivations[ state ]
-            visited.add( state )
 
             #------------------------------------------------
             # updating transition strings 
@@ -173,12 +186,127 @@ class CFG:
                 for st in tr:
                     a : bool = st in visited
                     b : bool = st in self.terminals
-                    if not ( a or b ):
+                    c : bool = st == EPSILON
+                    if not ( a or b or c ):
                         states.append( st )
+                        visited.add( st )
         
         return f"{name_header}\n\n{state_header}\n{termn_header}\n\n{transition_str}"
 
     ######## PARSING #########
+
+    def remove_leftr( self ):
+
+        '''
+        Re edits the grammar rules in order to remove left recursions.
+        This is necessary in order to guarantee that the top down parsing
+        of a token list wont enter in an infinite loop
+
+        for example:
+        
+        >>> print( G )
+        states = [ A , B ]
+        terminals = [ a , b , c , d ]
+
+        A :: B a | A a | c
+        B :: B b | A b | d
+
+        >>> G.remove_leftr()
+        >>> print( G )
+        
+        states = [ A , B , A* , B* ]
+        terminals = [ a , b , c , d ]
+
+        A :: B a A* | c A*
+        B :: c A* b B* | d B*
+        A* :: a A* | None 
+        B* :: b B* | a A* b B* | None
+        '''
+        
+        state_lst : state_seq = list( self.states )
+        foo : Callable = lambda x : len( self.derivations[ x ] )
+        state_lst.sort( key = foo )
+
+        rules_dict : Dict[ str , List[ state_seq ] ] = deepcopy( self.derivations )
+        new_states : state_set = set()
+        
+        n : int = len( state_lst )
+        for i in range( n ):
+
+            Si : str = state_lst[ i ]
+            rules : List[ state_seq ] = rules_dict[ Si ]
+
+            j : int = 0
+            while j <= i:
+
+                if j == i:
+                    #-------------------------------------------------------------
+                    # Replace any rule in the grammar with the format:
+                    # A :: A a | b
+                    #
+                    # By the new rules
+                    # A :: b A*
+                    # A* :: a A* | None
+                    #
+                    # Where b and a are sequences of symbols ( states and terminals )
+                    # that do not begin with the state A
+
+                    a_seqs : List[ state_lst ] = []
+                    b_seqs : List[ state_lst ] = []
+                    for rule in rules:
+                        if rule[ 0 ] == Si:
+                            a_seqs.append( rule[ 1: ] )
+                        else:
+                            b_seqs.append( rule )
+                    
+                    #------------------------------------------------------------
+                    # no imediate recursion
+                    if not a_seqs:
+                        j += 1
+                        continue
+                    
+                    #------------------------------------------------------------
+                    # All imediate recursions. No Idea on what to do
+                    if not b_seqs:
+                        pass
+                    
+                    new_states.add( Si + "*" )
+                    rules_dict[ Si ] = []
+                    for b_rule in b_seqs:
+                        rules_dict[ Si ].append( b_rule + [ Si + "*" ] )
+
+                    rules_dict[ Si + "*" ] = []
+                    for a_rule in a_seqs:
+                        rules_dict[ Si + "*" ].append( a_rule + [ Si + "*" ] )
+                    rules_dict[ Si + "*" ].append( [EPSILON] )
+
+                else:
+                    Sj = state_lst[ j ]
+
+                    #-----------------------------------------------------------
+                    # Replace any rule with the format.
+                    # Si :: Sj b
+                    #
+                    # by, for every a where Sj :: a:
+                    # Si :: a b
+                    bs : List[ state_seq ] = []
+                    for rule in rules:
+                        if rule[ 0 ] == Sj and len( rule ) > 1:
+                            rules.remove( rule )
+                            bs.append( rule[ 1: ] )
+                    
+                    if not bs:
+                        j += 1
+                        continue
+                        
+                    for b in bs:
+                        for a in rules_dict[ Sj ]:
+                            rules.append( a + b )
+                j += 1
+
+        self.left_recursion = False
+        self.derivations = rules_dict
+        self.states = self.states | new_states
 
     def parse( self , tok_seq : List[ str ] ) -> bool:
         
@@ -197,13 +325,19 @@ class CFG:
         if any( tok not in self.terminals for tok in tok_seq ):
             return False
         
+        #-----------------------------------------------------
+        # left recursions must be removed for efficient and
+        # reliable top down parsing
+        if self.left_recursion:
+            self.remove_leftr()
+
         return result
         
 
 if __name__ == "__main__":
 
+    G = CFG.from_file( "dummycfg.json" )
+    G.remove_leftr()
+    print( G )
 
-    f = open( 'tinycfg.json' , 'r')
-    G = CFG.from_json( f.read() )
-    f.close()
-    print( G ) 
+    
